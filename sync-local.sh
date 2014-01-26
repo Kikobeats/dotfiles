59bca4f47c45ac143d6a807f556a8899b98ac521
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-01-26 12:32:14 +0100

    added new sync method and refactor

---
+++ sync-local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+function doIt() {
+	rsync --exclude ".git/" --exclude ".gitignore" --exclude "install-deps.sh" --exclude ".DS_Store" --exclude "sync.sh" --exclude "readme.md" --exclude "init" --exclude "utils" --exclude "sync.sh" -av . ~
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
+source ~/.bash_profile
