3d381efd462c82ac84c2c48b8062eff2bd16c12f
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-03-27 09:56:39 +0100

    updated

--- install-deps.sh
+++ install-deps.sh
@@ -1,10 +1,6 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 sudo chmod -R +x ~/dotfiles
 
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
 # https://rvm.io
 # rvm for the rubiess
 curl -sSL https://get.rvm.io | bash -s stable
