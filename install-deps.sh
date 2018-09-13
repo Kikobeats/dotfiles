ea2770de94b9291762415ada8d547a6caf78a154
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-09-13 12:17:39 +0200

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -16,22 +16,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # z, oh how i love you
 curl https://raw.github.com/rupa/z/master/z.sh > ~/dotfiles/utils/z
 
-# I don't use Spotify... and you? :-)
-# cd ~/code
-# git clone git://github.com/dronir/SpotifyControl.git
-
-# add Dropbox uploader
-# curl https://raw2.github.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh > ~/dotfiles/utils/dropbox
-
 # fix spotlight
 curl -O https://fix-macosx.com/fix-macosx.py && /usr/bin/python fix-macosx.py
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures and into dropbox/public
-# then:
-# ln -s ~/Dropbox/Public/Photo\ Booth/ ~/Pictures/Photo\ Booth
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
 
 # iterm2 integration
 curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
