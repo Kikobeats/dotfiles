3eb4a56a5424dfd980a5bdc6e76facc3aef7e208
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-12-28 23:16:47 +0100

    load rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,9 @@ if status is-interactive
     # Initialize `zoxide`
     zoxide init fish | source
 
+    # load `rbenv`
+    rbenv init - fish | source
+
     if test -e ~/.extra
         source ~/.extra
     end
