0d796e1a9b6c6cd3fa8bd8e0a32230c48d12b874
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-12-05 12:29:41 +0100

    refactor: remove rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,6 @@
 if status is-interactive
     # Initialize `zoxide`
     zoxide init fish | source
-
-    # load `rbenv`
-    # rbenv init - fish | source
     if test -e ~/.extra
         source ~/.extra
     end
