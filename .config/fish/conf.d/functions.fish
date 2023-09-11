12fec8cd6ab4c5a692ee2c9b4148345e8d806292
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-09-11 21:39:38 +0200

    add spot function
    inspired by
    
    - https://github.com/junegunn/fzf/blob/master/ADVANCED.md?#ripgrep-integration
    - https://github.com/rauchg/spot

--- .config/fish/conf.d/functions.fish
+++ .config/fish/conf.d/functions.fish
@@ -1,3 +1,13 @@
+function spot
+    rg --color=always --line-number --no-heading --smart-case "$argv" |
+    fzf --ansi \
+    --color "hl:-1:underline,hl+:-1:underline:reverse" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --preview-window 'up,80%,border-bottom,+{2}+3/3,~3' \
+    --bind 'enter:become(code -g {1}:{2})'  
+end
+
 # support alias inside watch
 function w
     watch "fish -c '$argv'"
